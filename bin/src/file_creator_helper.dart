df08bedeade4cc7c9ffe030af0599a16e0b65967
Jordy De Jonghe <jordy.dejonghe@icapps.com>
2022-01-26 15:23:28 +0100

    format + sort imports

--- bin/src/file_creator_helper.dart
+++ bin/src/file_creator_helper.dart
@@ -8,11 +8,21 @@ import 'case_util.dart';
 class FileCreatorHelper {
   FileCreatorHelper._();
 
-  static void createViewModelFile(String screenName,
-      {required bool generateDI, required bool initFuture}) {
-    final sb = StringBuffer()
-      ..writeln(
-          "import 'package:icapps_architecture/icapps_architecture.dart';");
+  static List<String> getSortedImports(String file, List<String> extraImports) {
+    final imports = <String>[];
+    const LineSplitter().convert(file).forEach(
+      (l) {
+        if (l.startsWith('import ')) imports.add(l);
+      },
+    );
+    imports
+      ..addAll(extraImports)
+      ..sort();
+    return imports;
+  }
+
+  static void createViewModelFile(String screenName, {required bool generateDI, required bool initFuture}) {
+    final sb = StringBuffer()..writeln("import 'package:icapps_architecture/icapps_architecture.dart';");
     if (generateDI) {
       sb
         ..writeln("import 'package:injectable/injectable.dart';")
@@ -22,68 +32,56 @@ class FileCreatorHelper {
       sb.writeln();
     }
     sb
-      ..writeln(
-          'class ${CaseUtil.getCamelcase(screenName)}ViewModel with ChangeNotifierEx {')
-      ..writeln(
-          '  late final ${CaseUtil.getCamelcase(screenName)}Navigator _navigator;')
+      ..writeln('class ${CaseUtil.getCamelcase(screenName)}ViewModel with ChangeNotifierEx {')
+      ..writeln('  late final ${CaseUtil.getCamelcase(screenName)}Navigator _navigator;')
       ..writeln();
     if (initFuture) {
-      sb.writeln(
-          '  Future<void> init(${CaseUtil.getCamelcase(screenName)}Navigator navigator) async {');
+      sb.writeln('  Future<void> init(${CaseUtil.getCamelcase(screenName)}Navigator navigator) async {');
     } else {
-      sb.writeln(
-          '  void init(${CaseUtil.getCamelcase(screenName)}Navigator navigator) {');
+      sb.writeln('  void init(${CaseUtil.getCamelcase(screenName)}Navigator navigator) {');
     }
     sb
       ..writeln('    _navigator = navigator;')
       ..writeln('  }')
       ..writeln('}')
       ..writeln()
-      ..writeln(
-          'abstract class ${CaseUtil.getCamelcase(screenName)}Navigator {}');
+      ..writeln('abstract class ${CaseUtil.getCamelcase(screenName)}Navigator {}');
 
     // Write to file
-    File(join('lib', 'viewmodel', screenName, '${screenName}_viewmodel.dart'))
-        .writeAsStringSync(sb.toString());
+    File(join('lib', 'viewmodel', screenName, '${screenName}_viewmodel.dart')).writeAsStringSync(sb.toString());
   }
 
-  static void createScreenFile(String projectName, String screenName,
-      {required bool generateDI}) {
-    final sb = StringBuffer()
-      ..writeln("import 'package:flutter/material.dart';");
+  static void createScreenFile(String projectName, String screenName, {required bool generateDI}) {
+    final imports = [
+      "import 'package:flutter/material.dart';",
+      "import 'package:$projectName/navigator/route_names.dart';",
+      "import 'package:$projectName/viewmodel/$screenName/${screenName}_viewmodel.dart';",
+      "import 'package:$projectName/widget/provider/provider_widget.dart';",
+    ];
     if (generateDI) {
-      sb.writeln("import 'package:get_it/get_it.dart';");
+      imports.add("import 'package:get_it/get_it.dart';");
     }
-    sb
-      ..writeln("import 'package:$projectName/navigator/route_names.dart';")
-      ..writeln(
-          "import 'package:$projectName/viewmodel/$screenName/${screenName}_viewmodel.dart';")
-      ..writeln(
-          "import 'package:$projectName/widget/provider/provider_widget.dart';")
+    imports.sort();
+    final sb = StringBuffer()
+      ..writeAll(imports, '\n')
       ..writeln()
       ..writeln()
-      ..writeln(
-          'class ${CaseUtil.getCamelcase(screenName)}Screen extends StatefulWidget {')
-      ..writeln(
-          '  static const String routeName = RouteNames.${CaseUtil.getCamelcase(screenName, capitalizeFirstLetter: false)}Screen;')
+      ..writeln('class ${CaseUtil.getCamelcase(screenName)}Screen extends StatefulWidget {')
+      ..writeln('  static const String routeName = RouteNames.${CaseUtil.getCamelcase(screenName, capitalizeFirstLetter: false)}Screen;')
       ..writeln()
-      ..writeln(
-          '  const ${CaseUtil.getCamelcase(screenName)}Screen({Key? key}) : super(key: key);')
+      ..writeln('  const ${CaseUtil.getCamelcase(screenName)}Screen({Key? key}) : super(key: key);')
       ..writeln()
       ..writeln('  @override')
-      ..writeln(
-          '  _${CaseUtil.getCamelcase(screenName)}ScreenState createState() => _${CaseUtil.getCamelcase(screenName)}ScreenState();')
+      ..writeln('  _${CaseUtil.getCamelcase(screenName)}ScreenState createState() => _${CaseUtil.getCamelcase(screenName)}ScreenState();')
       ..writeln('}')
       ..writeln()
       ..writeln(
           'class _${CaseUtil.getCamelcase(screenName)}ScreenState extends State<${CaseUtil.getCamelcase(screenName)}Screen> implements ${CaseUtil.getCamelcase(screenName)}Navigator {')
       ..writeln('  @override')
       ..writeln('  Widget build(BuildContext context) {')
-      ..writeln(
-          '    return ProviderWidget<${CaseUtil.getCamelcase(screenName)}ViewModel>(')
+      ..writeln('    return ProviderWidget<${CaseUtil.getCamelcase(screenName)}ViewModel>(')
       ..writeln('      create: () => GetIt.I.get()..init(this),')
-      ..writeln(
-          '      childBuilderWithViewModel: (context, viewModel, theme, localization) => const Scaffold(')
+      ..writeln('      childBuilderWithViewModel: (context, viewModel, theme, localization) => const Scaffold(')
       ..writeln('        body: Center(),')
       ..writeln('      ),')
       ..writeln('    );')
@@ -91,88 +89,83 @@ class FileCreatorHelper {
       ..writeln('}');
 
     // Write to file
-    File(join('lib', 'screen', screenName, '${screenName}_screen.dart'))
-        .writeAsStringSync(sb.toString());
+    File(join('lib', 'screen', screenName, '${screenName}_screen.dart')).writeAsStringSync(sb.toString());
   }
 
-  static Future<void> updateMainNavigator(
-      String projectName, String screenName) async {
-    final mainNavigatorFile =
-        File(join('lib', 'navigator', 'main_navigator.dart'));
+  static Future<void> updateMainNavigator(String projectName, String screenName) async {
+    final mainNavigatorFile = File(join('lib', 'navigator', 'main_navigator.dart'));
     if (!mainNavigatorFile.existsSync()) {
-      print(
-          '`lib/navigator/main_navigator.dart` does not exists. Can not add navigation logic.');
+      print('`lib/navigator/main_navigator.dart` does not exists. Can not add navigation logic.');
       return;
     }
 
     final sb = StringBuffer();
     var writeOnGenerateRoute = false;
     var overrideMissing = false;
-    await mainNavigatorFile.readAsString().then(
-          (value) => const LineSplitter().convert(value).forEach(
-            (l) {
-              if (l == '  Route? onGenerateRoute(RouteSettings settings) {') {
-                writeOnGenerateRoute = true;
-              }
-              if (l == '      default:' && writeOnGenerateRoute) {
-                sb
-                  ..writeln(
-                      '      case ${CaseUtil.getCamelcase(screenName)}Screen.routeName:')
-                  ..writeln(
-                      '        return MaterialPageRoute(builder: (context) => const FlavorBanner(child: ${CaseUtil.getCamelcase(screenName)}Screen()), settings: settings);');
-              }
-              if (l ==
-                  '  void closeDialog<T>({T? result}) => Navigator.of(context, rootNavigator: true).pop(result);') {
-                overrideMissing = true;
-                sb
-                  ..writeln(
-                      '  void goTo${CaseUtil.getCamelcase(screenName)}() => navigationKey.currentState?.pushReplacementNamed(${CaseUtil.getCamelcase(screenName)}Screen.routeName);')
-                  ..writeln();
-              }
-              if (overrideMissing) {
-                overrideMissing = false;
-                sb.writeln('  @override');
-              }
-              sb.writeln(l);
-              if(l == "import 'package:icapps_generator_example/navigator/main_navigation.dart';"){
-               sb.writeln("import 'package:$projectName/screen/$screenName/${screenName}_screen.dart';");
-              }
-            },
-          ),
-        );
+    final fileString = await mainNavigatorFile.readAsString();
+    sb
+      ..writeAll(
+        getSortedImports(
+          fileString,
+          ["import 'package:$projectName/screen/$screenName/${screenName}_screen.dart';"],
+        ),
+        '\n',
+      )
+      ..writeln('');
+    const LineSplitter().convert(fileString).forEach(
+      (l) {
+        if (l == '  Route? onGenerateRoute(RouteSettings settings) {') {
+          writeOnGenerateRoute = true;
+        }
+        if (l == '      default:' && writeOnGenerateRoute) {
+          sb
+            ..writeln('      case ${CaseUtil.getCamelcase(screenName)}Screen.routeName:')
+            ..writeln('        return MaterialPageRoute(builder: (context) => const FlavorBanner(child: ${CaseUtil.getCamelcase(screenName)}Screen()), settings: settings);');
+        }
+        if (l == '  void closeDialog<T>({T? result}) => Navigator.of(context, rootNavigator: true).pop(result);') {
+          overrideMissing = true;
+          sb
+            ..writeln(
+                '  void goTo${CaseUtil.getCamelcase(screenName)}() => navigationKey.currentState?.pushReplacementNamed(${CaseUtil.getCamelcase(screenName)}Screen.routeName);')
+            ..writeln();
+        }
+        if (overrideMissing) {
+          overrideMissing = false;
+          sb.writeln('  @override');
+        }
+        if (!l.startsWith('import')) {
+          sb.writeln(l);
+        }
+      },
+    );
     mainNavigatorFile.writeAsStringSync(sb.toString());
   }
 
   static Future<void> updateRouteNames(String screenName) async {
     final routeNameFile = File(join('lib', 'navigator', 'route_names.dart'));
     if (!routeNameFile.existsSync()) {
-      print(
-          '`lib/navigator/route_names.dart` does not exists. Can not add navigation logic.');
+      print('`lib/navigator/route_names.dart` does not exists. Can not add navigation logic.');
       return;
     }
 
     final sb = StringBuffer();
     await routeNameFile.readAsString().then(
           (value) => const LineSplitter().convert(value).forEach(
-                (l) {
-                  sb.writeln(l);
-                  if (l == '  RouteNames._();') {
-                    sb.writeln(
-                        "  static const ${CaseUtil.getCamelcase(screenName, capitalizeFirstLetter: false)}Screen = '$screenName';");
-                  }
-                },
-              ),
+            (l) {
+              sb.writeln(l);
+              if (l == '  RouteNames._();') {
+                sb.writeln("  static const ${CaseUtil.getCamelcase(screenName, capitalizeFirstLetter: false)}Screen = '$screenName';");
+              }
+            },
+          ),
         );
     routeNameFile.writeAsStringSync(sb.toString());
   }
 
-  static Future<void> updateMainNavigation(
-      String projectName, String screenName) async {
-    final mainNavigationFile =
-        File(join('lib', 'navigator', 'main_navigation.dart'));
+  static Future<void> updateMainNavigation(String projectName, String screenName) async {
+    final mainNavigationFile = File(join('lib', 'navigator', 'main_navigation.dart'));
     if (!mainNavigationFile.existsSync()) {
-      print(
-          '`lib/navigator/main_navigation.dart` does not exists. Can not add navigation logic.');
+      print('`lib/navigator/main_navigation.dart` does not exists. Can not add navigation logic.');
       return;
     }
     final sb = StringBuffer();
@@ -183,8 +176,7 @@ class FileCreatorHelper {
               if (l == '  void goBack<T>({T? result});') {
                 sb
                   ..writeln()
-                  ..writeln(
-                      '  void goTo${CaseUtil.getCamelcase(screenName)}();');
+                  ..writeln('  void goTo${CaseUtil.getCamelcase(screenName)}();');
               }
             },
           ),
